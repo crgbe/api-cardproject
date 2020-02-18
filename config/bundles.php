63f8919a7724d4a5873ae40e1a3d03425df37d08
crgbe <credo.gbededji@gmail.com>
2020-02-18 18:25:18 +0100

    Installation du bundle Security et création de l'entité User. Création de la classe Token Authenticator and implémentation des méthodes provenant de AbstractGuardAuthenticator

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
