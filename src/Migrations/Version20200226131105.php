4878e380675078029d27b6d4c38b33906fa1f1d4
crgbe <credo.gbededji@gmail.com>
2020-02-26 15:08:32 +0100

    Création de l'entité 'Family' et du controlleur des webservices CRUD

---
+++ src/Migrations/Version20200226131105.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200226131105 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE card_card_group DROP FOREIGN KEY FK_3986280D476517C4');
+        $this->addSql('CREATE TABLE family (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE card_card_group');
+        $this->addSql('DROP TABLE card_group');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE card_card_group (card_id INT NOT NULL, card_group_id INT NOT NULL, INDEX IDX_3986280D4ACC9A20 (card_id), INDEX IDX_3986280D476517C4 (card_group_id), PRIMARY KEY(card_id, card_group_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE card_group (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE card_card_group ADD CONSTRAINT FK_3986280D476517C4 FOREIGN KEY (card_group_id) REFERENCES card_group (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE card_card_group ADD CONSTRAINT FK_3986280D4ACC9A20 FOREIGN KEY (card_id) REFERENCES card (id) ON DELETE CASCADE');
+        $this->addSql('DROP TABLE family');
+    }
+}
